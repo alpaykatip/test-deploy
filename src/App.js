8a23d6a894e3bd0b66ec6e490252715adc33b92d
John Harley <info@designvice.co.uk>
2022-01-14 12:22:04 +0000

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+      maxime sint, natus veritatis impedit quae corrupti sunt aut soluta cum
+      voluptas perspiciatis ratione ducimus id? Sapiente corporis quam odit.
+      Harum numquam illo, hic, fugit architecto magni sunt eum sed dicta facere
+      possimus autem dolore, unde nesciunt. Facilis voluptatibus dolor hic?
     </div>
   );
 }
